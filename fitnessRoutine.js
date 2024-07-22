bbb29b3ad63672cfd877ad990fc422e98b989190
Andres Gomez <andresxv98@gmail.com>
2024-07-22 14:13:07 -0500

    Complete program

--- fitnessRoutine.js
+++ fitnessRoutine.js
@@ -4,7 +4,24 @@ Muscle group to work today:
 Sets & reps:
 Excercises: 
 
-hola 
-
 */
+// Crear un objeto con 3 array nested dentro de el, de donde se tomaran los datos
+const routine = {
+    muscle: ["Biceps", "Legs", "Chest"],
+    setsAndReps : ["3x12", "4x15", "3x6", "2x20", "5x3"],
+    excercises: ["Dumbbells Curl", "Hack Squat", "Bench Press"]
+};
+// Paso 2: Generar un índice aleatorio
+randomIndex = Math.floor(Math.random() * routine.muscle.length);
+
+// Paso 3: Seleccionar elementos aleatorios usando el índice
+const arr1 = routine.muscle[randomIndex];
+const arr2 = routine.setsAndReps[randomIndex];
+const arr3 = routine.excercises[randomIndex];
+
+// Paso 4: Construir y mostrar el mensaje
+const message = `Today's routine is for ${arr1}, sets and reps: ${arr2} and the excercise is: ${arr3}`;
+
+console.log(message);
+
 
