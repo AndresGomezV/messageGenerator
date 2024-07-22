c1fb2556519464d45ecbf43eac83531a54c54304
Andres Gomez <andresxv98@gmail.com>
2024-07-22 15:21:06 -0500

    complete project

--- fitnessRoutine.js
+++ fitnessRoutine.js
@@ -7,17 +7,19 @@ Excercises:
 */
 // Crear un objeto con 3 array nested dentro de el, de donde se tomaran los datos
 const routine = {
-    muscle: ["Biceps", "Legs", "Chest"],
+    muscle: ["Cardio"],
     setsAndReps : ["3x12", "4x15", "3x6", "2x20", "5x3"],
-    excercises: ["Dumbbells Curl", "Hack Squat", "Bench Press"]
+    excercises: ["Dumbbells Curl", "Prono", "Supino"]
 };
 // Paso 2: Generar un índice aleatorio
-randomIndex = Math.floor(Math.random() * routine.muscle.length);
+randomIndexMuscle = Math.floor(Math.random() * routine.muscle.length);
+randomIndexsetsAndReps = Math.floor(Math.random() * routine.setsAndReps.length);
+randomIndexExcercises = Math.floor(Math.random() * routine.excercises.length);
 
 // Paso 3: Seleccionar elementos aleatorios usando el índice
-const arr1 = routine.muscle[randomIndex];
-const arr2 = routine.setsAndReps[randomIndex];
-const arr3 = routine.excercises[randomIndex];
+const arr1 = routine.muscle[randomIndexMuscle];
+const arr2 = routine.setsAndReps[randomIndexsetsAndReps];
+const arr3 = routine.excercises[randomIndexExcercises];
 
 // Paso 4: Construir y mostrar el mensaje
 const message = `Today's routine is for ${arr1}, sets and reps: ${arr2} and the excercise is: ${arr3}`;
